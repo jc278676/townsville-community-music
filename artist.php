467f0a2aa33e3aeec4c9a759c9794a1f065e7e3d
Rainbowturn1p <joshua.farleysmith@my.jcu.edu.au>
2015-04-19 12:35:54 +1000

    Duplicated example files
    Named them simply to avoid confusion. Renamed all pages to make them
    relevant with correct linking. SQLite names still need to be changed.

---
+++ artist.php
@@ -0,0 +1,66 @@
+<?php
+include("dbconnectartist.php")
+/* Fairly simple example - there's a form for inserting a new phone record and a set of forms, one for each record,
+	that allows for deleting and updating each record. In these ones, the id of the record is passed using a hidden form field. 
+*/
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>PHP SQLite Database Example (Artist Records)</title>
+<style type="text/css">
+.subtleSet {
+	border-radius:25px;
+	width: 30em;
+}
+.deleteButton {
+	color: red;
+}
+</style>
+</head>
+
+<body>
+<h1>Artist Database</h1>
+<form id="insert" name="insert" method="post" action="dbprocessartist.php">
+<fieldset class="subtleSet">
+    <h2>Insert new artist:</h2>
+    <p>
+      <label for="name">Name: </label>
+      <input type="text" name="name" id="name">
+    </p>
+    <p>
+      <label for="phone">Phone: </label>
+      <input type="text" name="phone" id="phone">
+    </p>
+    <p>
+      <input type="submit" name="submit" id="submit" value="Insert Entry">
+    </p>
+</fieldset>
+</form>
+
+<fieldset class="subtleSet">
+<h2>Current data:</h2>
+<?php
+// Display what's in the database at the moment.
+$sql = "SELECT * FROM people";
+foreach ($dbh->query($sql) as $row)
+{
+?>
+<form id="deleteForm" name="deleteForm" method="post" action="dbprocessartist.php">
+<?php
+	echo "<input type='text' name='name' value='$row[name]' /> <input type='text' name='phone' value='$row[phone]' />\n";
+	echo "<input type='hidden' name='id' value='$row[id]' />";
+?>
+<input type="submit" name="submit" value="Update Entry" />
+<input type="submit" name="submit" value="Delete Entry" class="deleteButton">
+<input type="submit" name="submit" value="X" class="deleteButton">
+</form>
+<?php
+}
+echo "</fieldset>\n";
+// close the database connection
+$dbh = null;
+?>
+</body>
+</html>
