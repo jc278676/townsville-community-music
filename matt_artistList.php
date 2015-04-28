0513d95027d612c226a7963a24563db52c00ed7b
Matt Livingston <matthew.livingston@my.jcu.edu.au>
2015-04-28 11:04:48 +1000

    Reduced artist list page and created artist details page. Minor styling changes along the way.

--- matt_artistList.php
+++ matt_artistList.php
@@ -15,37 +15,29 @@ include("dbconnectartist.php")
 
 
 <h1>Featured Artists</h1>
-
 <?php
-// Display what's in the database at the moment.
-
+// Grab our list of artists from the database
+// $query returns an array of associative arrays
 $query = $dbh->prepare('SELECT * FROM artists');
 $query->execute();
-
 $rows= $query -> fetchAll();
 
-$rowCount = 0;
-
+// Random ordering is fine for now
 shuffle($rows);
 foreach ($rows as $row)
 {
-    if ($rowCount % 2 == 0) {
-        $photoClass = "artistPhotoRight";
-    } else {
-        $photoClass = "artistPhotoLeft";
-    }
-    //
-    echo "<div><h2>$row[artistName]</h2><img src='musos/$row[artistPhoto]' class='$photoClass'><p>";
-
-    echo str_replace("\n", "</p><p>", $row[artistText]);
-    echo "</p>";
+    echo "<div>";
+    echo "<h2>$row[artistName]</h2>";
+    echo "<a href='matt_artistDisplay.php?artistId=$row[artistId]'><img src='musos/thumb$row[artistPhoto]' class='artistThumbLeft'></a>";
+    echo "Genre: $row[artistGenre]<br>";
+    echo "<br>";
+    echo "Brief (1 or 2 lines) description of this artist from the database <br>(currently does not exist in db) <br>";
+    echo "<br>";
+    echo "<a href='matt_artistDisplay.php?artistId=$row[artistId]'>More about $row[artistName]</a>";
     echo "</div>";
-    // switch sides
-    $rowCount += 1;
 }
 // close the database connection
 $dbh = null;
 ?>
-</table>
 </body>
 </html>
