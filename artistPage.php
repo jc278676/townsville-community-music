4f0c61ebbd520b35147a6cdd58e83b3581f03342
Matt Livingston <matthew.livingston@my.jcu.edu.au>
2015-05-15 07:25:32 +1000

    Blended artistList.php artistDisplay.php and artistPage.html into artistPage.php. Code formatting changes to artistPage.html.  Typos fixed in tcmcStyle.css.

---
+++ artistPage.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+<link href="styles/tcmcStyle.css" rel="stylesheet" type="text/css">
+</head>
+
+<body><div id="colourHeader"></div><div id="siteCenter">
+<div class="header">
+<ul><li><a href="artistPage.html"><img src="logo.png" width="141" height="100" alt="Home"/></a></li>
+<li><a href="artistPage.html">Events</a></li>
+<li><a href="artistPage.html">Bulletin Board</a></li>
+<li><a href="artistPage.html">Musicians</a></li>
+<li><a href="artistPage.html">Membership</a></li>
+<li><a href="artistPage.html">About</a></li>
+<li><a href="artistPage.html">Feeder</a></li>
+</ul>
+<div class="center"><p><a href="artistPage.html">Free listing for Music Centre members!<br>
+Publicise yourself, your band or organisation here!</a></p></div>
+</div>
+<div id="contentLeft"><div class="upcomingShows"><h2>UPCOMING SHOWS</h2>
+</div>
+<div class="artist"><h2>ARTISTS</h2>
+<div class="featureArtist"><h1>PRETEND I AM AN ARTIST</h1><p>This maybe one information, but this is not information. It may look good or help you, but it is not information. This maybe one information, but this is not information. It may look good or help you, but it is not information.This maybe one information, but this is not information. It may look good or help you, but it is not information.This maybe one information, but this is not information. It may look good or help you, but it is not information.This maybe one information, but this is not information. It may look good or help you, but it is not information.This maybe one information, but this is not information. It may look good or help you, but it is not information.This maybe one information, but this is not information. It may look good or help you, but it is not information.This maybe one information, but this is not information. It may look good or help you, but it is not information.This maybe one information, but this is not information. It may look good or help you, but it is not information. Bye</p></div></div></div>
+ 
+<div id="contentRight"><h2>ARTISTS</h2><div id="artistList">
+<ul><li><a href="artistPage.html">Musician01</a></li>
+<li><a href="artistPage.html">Musician02</a></li>
+<li><a href="artistPage.html">Musician03</a></li>
+<li><a href="artistPage.html">Musician04</a></li>
+</ul></div></div>
+</div>
+<div id="footer">
+</div></body>
+</html>
