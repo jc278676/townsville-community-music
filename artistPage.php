b3b74aa902ee18a75a5fe744ac74e8a67d087d36
Rainbowturn1p <joshua.farleysmith@my.jcu.edu.au>
2015-05-19 11:46:26 +1000

    Created PHP version of HTML pages
    Created files that I could work on that replicate the current design
    team files

---
+++ artistPage.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+<link href="tmcStyle.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div id="colourHeader">
+</div>
+<div id="siteCenter">
+	<div class="header">
+		<div class="member">
+        	<ul>
+            	<li><img src="buttonUp.gif" width="80" height="17" alt=""/></li>
+                <li><img src="buttonUp.gif" width="80" height="17" alt=""/></li>
+            </ul>
+        </div>
+		    <ul>
+                <a href="home.html"><img src="logo.png" width="141" height="100" alt="Home"/></a>
+			    <li><a href="home.html">Home</a></li>
+			    <li><a href="eventPage.html">Events</a></li>
+			    <li><a href="bulletinBoardPage.html">Bulletin Board</a></li>
+			    <li class="currentHeaderSection">Musicians</li>
+			    <li><a href="membershipPage.html">Membership</a></li>
+			    <li><a href="aboutUs.html">About</a></li>
+			    <li><a href="artistPage.html">Feeder</a></li>
+		    </ul>
+		<div class="center">
+        	<p>
+            	<a href="artistPage.html">Free listing for Music Centre members!<br>Publicise yourself, your band or organisation here!</a>
+            </p>
+        </div>
+	</div>
+	<div id="contentLeft">
+		<div class="artist">
+        	<h2>ARTISTS</h2>
+			<div class="featureArtist">
+        		<h1>PRETEND I AM AN ARTIST</h1>
+            		<p>
+                		This maybe one information, but this is not information. It may look good or help you, but it is not information. This maybe one information, but this is not information. It may look good or help you, but it is not information.This maybe one information, but this is not information. It may look good or help you, but it is not information.This maybe one information, but this is not information. It may look good or help you, but it is not information.This maybe one information, but this is not information. It may look good or help you, but it is not information.This maybe one information, but this is not information. It may look good or help you, but it is not information.This maybe one information, but this is not information. It may look good or help you, but it is not information.This maybe one information, but this is not information. It may look good or help you, but it is not information.This maybe one information, but this is not information. It may look good or help you, but it is not information.
+                	</p>
+            </div>
+         </div>
+	</div>
+	<div id="contentRight">
+    	<h2>ARTISTS</h2>
+        <div id="artistList">
+		<ul>
+        	<li><a href="artistPage.html">Musician01</a></li>
+			<li><a href="artistPage.html">Musician02</a></li>
+			<li><a href="artistPage.html">Musician03</a></li>
+			<li><a href="artistPage.html">Musician04</a></li>
+		</ul>
+        </div>
+    </div>
+</div>
+<div id="footer">
+    <div class="footerContent">
+        <div class="sponsorImg">    
+            <img src="TCCgcbfQN100px.png" width="500" height="100" alt="sponsors"/>
+        </div>
+    </div>
+</div>
+</body>
+</html>
