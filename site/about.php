0f5d9652e7c758f3dedc26a7829fb99b58c97668
Matt Livingston <matthew.livingston@my.jcu.edu.au>
2015-05-22 14:48:14 +1000

    merged imaged into site/

--- site/about.php
+++ site/about.php
@@ -49,18 +49,41 @@ Every web page has the following structure:
     ?>
 
     <div id="content">
+        <h2>About</h2>
+        <p class="borderLine">
         <div class="event">
             <div class="information">
-                <h1>This div grows to match the available content.</h1>
-                <p>
-                    BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER A SNAKE! A SNAKE! OOOH IT'S A SNAKE! BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOMBADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOMBADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER
-                    BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER A SNAKE! A SNAKE! OOOH IT'S A SNAKE! BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOMBADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOMBADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER
-                    BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER A SNAKE! A SNAKE! OOOH IT'S A SNAKE! BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOMBADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOMBADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER
-                    BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER A SNAKE! A SNAKE! OOOH IT'S A SNAKE! BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOMBADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOMBADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER BADGER MUSHROOM MUSHROOM BADGER BADGER BADGER BADGER BADGER BADGER BADGER
-                </p>
-            </div class="information">
-        </div class="event">
-    </div id="content">
+                <img src="CivicFront300.jpg" width="300" height="170" alt="Civic Centre">
+                <strong>Contact Details:-</strong> <br>
+                <br>
+                <strong>Phone</strong> &nbsp;&nbsp;&nbsp;&nbsp;07 4724 2086<br>
+                <strong>Mobile</strong> &nbsp;&nbsp;&nbsp;402 255 182<br>
+                <strong>Email</strong> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;admin@townsvillemusic.org.au<br>
+                <br>
+                <strong>Postal Address</strong> &nbsp;&nbsp;&nbsp; PO Box 1006, Townsville, QLD 4810<br>
+                <strong>Address</strong> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Townsville Civic Theatre, 41 Boundary Street, Townsville, QLD 4810<br>
+                <strong>Office Hours</strong> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 9.30 -2.30 Monday - Wednesday
+            </div>
+        </p>
+        <p class="borderLine">
+        <div class="information">
+            <h3>About Us</h3>
+            Townsville Community Music Centre is a not-for-profit, incorporated organisation serving musicians and their audience in North Queensland. The Music Centre's role includes facilitation, presentation and promotion of events, functions, education, workshops, concerts, festivals in Townsville, and surrounding centres.<br>
+            <br>
+            Based in Townsville, North Qld, the Music Centre presents concerts and workshops throughout the year, in a diverse range of genres including classical, jazz, folk, blues, world and contemporary music, featuring touring artists and locally-based professional and emerging artists. It is involved in the instigation of projects based on cross collaboration with other artistic disciplines, and it promotes the work of North Queensland musicians outside the region, organising tours and promoting their work.
+        </div>
+        </p>
+        <p class="borderLine">
+        <div class="information">
+            <h3>Looking Back - Historic Collections </h3>
+            Townsville Community Music Centre has been constantly changing over the years to keep up to date with the musical tastes and needs of the Townsville community. As part of the relocation of the Music Centre to the Civic Theatre, Bronia Renison and Jean Dartnall, both librarians, have assessed the old collection of sheet music, books and recorded music which the centre has been storing, unused, for many years. Sometimes older things have to be discarded to make way for the new, but the Music Centre is aware that older material may still have value.<br>
+            The National Library of Australia has an online catalogue (TROVE) that lists not only its own holdings but also information about items held by many other libraries around Australia. Using this catalogue Bronia and Jean have identified at least 150 items of music that are not held by any of the country's major libraries. These items have been donated to the National Library to include in their collection and thus made available to all historians and musicians.<br>
+            <br>
+            Also discovered in the old collection were some pieces relevant to North Queensland. Local musicians performed these at a musical social afternoon on Sunday April 21st in C2 at the Civic Theatre. The remaining sheet music, books and CDs were put on display and distributed free of charge to the local music community.
+        </div>
+    </div>
+    </p>
+</div>
 
 </div id="siteCenter">
 
